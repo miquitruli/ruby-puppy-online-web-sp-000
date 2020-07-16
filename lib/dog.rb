37a2eab45446d1d1c72c5cbd940ade6113b492bc
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-07-16 20:28:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 # Add your code here
 class Dog
   @@all=[]
-  attr_accessor :name, :save
-  def initialize(name, save)
+  attr_accessor :name
+  def initialize(name)
     @name=name
     @@all<<self
-    @save=save
   end
   def self.all
     @@all
